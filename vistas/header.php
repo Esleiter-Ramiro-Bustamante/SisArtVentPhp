247610c8319e468e2fa533a3cba03a32ce749ea2
Esleiter-Ramiro-Bustamante <esletutuns0307@gmail.com>
2018-04-18 03:40:09 -0500

    v1.2
    trabajando con la tabla articulos

--- vistas/header.php
+++ vistas/header.php
@@ -32,9 +32,9 @@
         <!-- Logo -->
         <a href="index2.html" class="logo">
           <!-- mini logo for sidebar mini 50x50 pixels -->
-          <span class="logo-mini"><b>{Code}Panda</b>Ventas</span>
+          <span class="logo-mini"><b>SVA-PHP</b></span>
           <!-- logo for regular state and mobile devices -->
-          <span class="logo-lg"><b>{Code}Panda</b></span>
+          <span class="logo-lg"><b>SVA-PHP</b></span>
         </a>
 
         <!-- Header Navbar: style can be found in header.less -->
